14a93a2741f486e5159cf72783b3452c4663b7ae
TaCongNguyen <45918144+TaCongNguyen@users.noreply.github.com>
2021-06-04 13:28:25 +0700

    update

--- GenMusic/app/src/main/java/com/example/genmusic/Fragment_Play_DanhsachbaiHat.java
+++ GenMusic/app/src/main/java/com/example/genmusic/Fragment_Play_DanhsachbaiHat.java
@@ -1,2 +1,32 @@
-package com.example.genmusic;public class Fragment_Play_DanhsachbaiHat {
+package com.example.genmusic;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Fragment_Play_DanhsachbaiHat extends Fragment {
+    View view;
+    RecyclerView recyclerViewplaynhac;
+    PlaynhacAdapter playnhacAdapter;
+    @Nullable
+
+    @Override
+    public View onCreateView(@NonNull  LayoutInflater inflater,@Nullable  ViewGroup container, @Nullable  Bundle savedInstanceState) {
+       view=inflater.inflate(R.layout.fragmentplaydanhsachbaihat,container,false);
+        recyclerViewplaynhac=view.findViewById(R.id.recyclerviewPlaybaihat);
+       if(PlayNhacActivity.mangbaihat.size()>0){
+
+           playnhacAdapter=new PlaynhacAdapter(getActivity(),PlayNhacActivity.mangbaihat);
+           recyclerViewplaynhac.setLayoutManager(new LinearLayoutManager(getActivity()));
+           recyclerViewplaynhac.setAdapter(playnhacAdapter);
+       }
+       return view;
+    }
 }
--- GenMusic/app/src/main/java/com/example/genmusic/PlayNhacActivity.java
+++ GenMusic/app/src/main/java/com/example/genmusic/PlayNhacActivity.java
@@ -1,10 +1,17 @@
 package com.example.genmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.genmusic.bxhFragment.Baihatuathich;
@@ -12,22 +19,69 @@ import com.example.genmusic.bxhFragment.Baihatuathich;
 import java.util.ArrayList;
 
 public class PlayNhacActivity extends AppCompatActivity {
-
+    Toolbar toolbarplaynhac;
+    TextView txtTimesong,txtTotaltimesong;
+    SeekBar sktime;
+    ImageButton imgplay,imgreapeat,imgnext,impre,imgrandom;
+    ViewPager viewPagerplaynhac;
+    public static ArrayList<Baihatuathich> mangbaihat=new ArrayList<>();
+    public static ViewPagerPlaylistnhac adapternhac;
+    Fragment_Dia_nhac fragment_dia_nhac;
+    Fragment_Play_DanhsachbaiHat fragment_play_danhsachbaiHat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_nhac);
+        init();
+        GetDataFromIntent();
+
+    }
+
+    private void GetDataFromIntent() {
         Intent intent=getIntent();
-        if(intent.hasExtra("cakhuc")){
-            Baihatuathich baihatuathich=intent.getParcelableExtra("cakhuc");
-            Toast.makeText(this,baihatuathich.getTenbaihat(),Toast.LENGTH_SHORT).show();
-        }
-        if(intent.hasExtra("cacbaihat")){
-            ArrayList<Baihatuathich> mangbaihat=intent.getParcelableArrayListExtra("cacbaihat");
-            for(int i=0;i<mangbaihat.size();i++)
-            {
-                Log.d("BBB",mangbaihat.get(i).getTenbaihat());
+        mangbaihat.clear();
+        if(intent!=null)
+        {
+            if(intent.hasExtra("cakhuc")){
+                Baihatuathich baihatuathich=intent.getParcelableExtra("cakhuc");
+                mangbaihat.add(baihatuathich);
+            }
+            if(intent.hasExtra("cacbaihat")){
+                ArrayList<Baihatuathich> baihaArrayList=intent.getParcelableArrayListExtra("cacbaihat");
+                mangbaihat=baihaArrayList;
             }
         }
+
+
+    }
+
+    private void init() {
+        toolbarplaynhac=findViewById((R.id.toolbarplaynhac));
+        txtTimesong=findViewById(R.id.textviewtimesong);
+        txtTotaltimesong=findViewById(R.id.textviewtotaltimesong);
+        sktime =findViewById(R.id.seekbarsong);
+        imgplay=findViewById(R.id.imagebuttonplay);
+        imgreapeat=findViewById(R.id.imagebuttonrepeat);
+        imgnext=findViewById(R.id.imagebuttonnext);
+        imgrandom=findViewById(R.id.imagebuttonsuffle);
+        impre=findViewById(R.id.imagebuttonpre);
+        viewPagerplaynhac=findViewById(R.id.viewpagerplaynhac);
+        setSupportActionBar(toolbarplaynhac);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbarplaynhac.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        toolbarplaynhac.setTitleTextColor(Color.WHITE);
+        fragment_dia_nhac=new Fragment_Dia_nhac();
+        fragment_play_danhsachbaiHat=new Fragment_Play_DanhsachbaiHat();
+        adapternhac= new ViewPagerPlaylistnhac((getSupportFragmentManager()));
+        adapternhac.AddFragment(fragment_play_danhsachbaiHat);
+        adapternhac.AddFragment(fragment_dia_nhac);
+
+        viewPagerplaynhac.setAdapter(adapternhac);
+
     }
 }
